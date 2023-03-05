7bdda0004d50820f52b526c1c9751bd652f285d3
AlinaOryshak <alinaoryshak@gmail.com>
2023-03-05 18:30:08 +0200

    first commit

---
+++ src/test/java/mis/medicalinformationsystem/MedicalInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mis.medicalinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalInformationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
