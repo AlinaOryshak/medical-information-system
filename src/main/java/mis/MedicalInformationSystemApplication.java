26431c4320de54b06e14ba58aea62c22c3edf557
AlinaOryshak <alinaoryshak@gmail.com>
2023-03-05 18:55:36 +0200

    change groupId

--- src/main/java/mis/MedicalInformationSystemApplication.java
+++ src/main/java/mis/MedicalInformationSystemApplication.java
@@ -1,4 +1,4 @@
-package mis.medicalinformationsystem;
+package mis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/mis/MedicalInformationSystemApplicationTests.java
+++ src/test/java/mis/MedicalInformationSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package mis.medicalinformationsystem;
+package mis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
